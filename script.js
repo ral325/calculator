2116007b22d3570b4ca422e489899a147915b7cc
Robert Lipton <ral325@cornell.edu>
2021-11-30 19:07:15 -0800

    initial commit. using flexbox, need to use grid

---
+++ script.js
@@ -0,0 +1,30 @@
+// calculator basic functions
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return (b === 0 ? "ERROR" : a/b);
+}
+
+function operate(operator,a,b) {
+    switch (operator){
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "*":
+            return multiply(a,b);
+        case "/":
+            return divide(a,b);
+    }
+}
+
